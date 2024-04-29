05b2d9cd82f3d284d90e965b0395cd9d6880579f
Karliss <>
2024-04-29 23:39:22 +0300

    Remove one more GCC in path

--- scripts/build_win.sh
+++ scripts/build_win.sh
@@ -2,7 +2,7 @@ set -euo pipefail
 which cl
 which gcc
 pacman -Q
-export PATH=`echo $PATH | tr ":" "\n" | grep -v "mingw64" | tr "\n" ":"`
+export PATH=`echo $PATH | tr ":" "\n" | grep -v "mingw64" | grep -v "Strawberry" | tr "\n" ":"`
 echo $PATH
 echo "checking gcc2"
 which gcc
