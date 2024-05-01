3377a6a9a46ed105ac8d30cb2f8ed234eaedaaec
Karliss <>
2024-05-01 23:27:22 +0300

    Use the preinstalled python.

--- scripts/build_win.sh
+++ scripts/build_win.sh
@@ -1,8 +1,8 @@
 set -euo pipefail
 which cl
 which gcc
-pacman -Q
+#pacman -Q
 export PATH=`echo $PATH | tr ":" "\n" | grep -v "mingw64" | grep -v "Strawberry" | tr "\n" ":"`
 echo $PATH
 which gcc || echo "No GCC in path, OK!"
-make PLATFORM=win
+make PLATFORM=win "PYTHON_WINDOWS=C:\\hostedtoolcache\\windows\\Python\\3.11.9\\x64\\"
