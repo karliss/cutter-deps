3db9475168bce6d336df789bc168b0f5ff7a5ade
Karliss <>
2024-05-06 22:35:30 +0300

    Fix checksum check.

--- scripts/build_win.sh
+++ scripts/build_win.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 LLVM_NAME=clang+llvm-18.1.5-x86_64-pc-windows-msvc
 LLVM_ARCHIVE="$LLVM_NAME.tar.xz"
 wget https://github.com/llvm/llvm-project/releases/download/llvmorg-18.1.5/$LLVM_ARCHIVE
-"7027f03bcab87d8a72fee35a82163b0730a9c92f5160373597de95010f722935  ./$LLVM_ARCHIVE" | sha256sum -c -
+echo "7027f03bcab87d8a72fee35a82163b0730a9c92f5160373597de95010f722935  ./$LLVM_ARCHIVE" | sha256sum -c -
 tar -xf $LLVM_ARCHIVE
 export CMAKE_PREFIX_PATH=$LLVM_NAME
 
