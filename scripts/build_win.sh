9039b6d8b0b8ba2cd184c17177ed46f0640566f3
Karliss <>
2024-05-06 22:41:48 +0300

    Tweak LLVM path.

--- scripts/build_win.sh
+++ scripts/build_win.sh
@@ -2,10 +2,10 @@ set -euo pipefail
 
 LLVM_NAME=clang+llvm-18.1.5-x86_64-pc-windows-msvc
 LLVM_ARCHIVE="$LLVM_NAME.tar.xz"
-wget https://github.com/llvm/llvm-project/releases/download/llvmorg-18.1.5/$LLVM_ARCHIVE
+wget --progress=dot:giga https://github.com/llvm/llvm-project/releases/download/llvmorg-18.1.5/$LLVM_ARCHIVE
 echo "7027f03bcab87d8a72fee35a82163b0730a9c92f5160373597de95010f722935  ./$LLVM_ARCHIVE" | sha256sum -c -
 tar -xf $LLVM_ARCHIVE
-export CMAKE_PREFIX_PATH=$LLVM_NAME
+export CMAKE_PREFIX_PATH=$PWD/$LLVM_NAME
 
 
 # REMOVE any gcc installs (possibly provided by msys) from path, we are trying to do a MSVC based build
